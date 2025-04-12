2f9bedde7ef3df7c9d11c4c37dcbccf7b9d06b16
Ishanshastrithecoder <irshastri30126@gmail.com>
2025-04-12 14:02:55 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import CareerAssistantPage from "./pages/CareerAssistantPage";
+import ResumeBuilderPage from "./pages/ResumeBuilderPage";
+import JobStatisticsPage from "./pages/JobStatisticsPage";
+import NetworkPage from "./pages/NetworkPage";
+import DashboardPage from "./pages/DashboardPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/career-assistant" element={<CareerAssistantPage />} />
+            <Route path="/resume-builder" element={<ResumeBuilderPage />} />
+            <Route path="/job-statistics" element={<JobStatisticsPage />} />
+            <Route path="/network" element={<NetworkPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
+export default App;
